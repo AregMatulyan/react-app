db6914b0a8ff22d80e207f11af2a0d7520e56269
Areg Matulyan <areg.matulyan@multisafepay.com>
2023-04-20 23:10:44 +0200

    Add product search

--- src/pages/product/list.js
+++ src/pages/product/list.js
@@ -1,13 +1,15 @@
-import React, {useEffect, useState} from 'react';
+import React, {useEffect, useState, useMemo} from 'react';
 import { useNavigate } from "react-router-dom";
 import { routes } from "../../router";
 import DummyJsonClient from '../../services/api/apiClient';
 import { DataGrid } from '@mui/x-data-grid';
-import { Typography, Box } from '@mui/material';
+import { Typography, Box, TextField } from '@mui/material';
+import debounce from 'lodash.debounce';
 
 const ProductListPage = () => {
     const [isLoading, setIsLoading] = useState(true);
     const [products, setProducts] = useState([]);
+    const [search, setSearch] = useState('');
     const [total, setTotal] = useState(0);
     const navigate = useNavigate();
     const cells = process.env.REACT_APP_PRODUCT_LIST_CELLS.split(',');
@@ -19,7 +21,7 @@ const ProductListPage = () => {
 
     const listProducts = (page) => {
         const skip = page * pageSize
-        DummyJsonClient.products.list(skip)
+        DummyJsonClient.products.list(search, skip)
             .then(res => {
                 setProducts(res.data.products);
                 setPaginationModel(prev => ({
@@ -31,9 +33,21 @@ const ProductListPage = () => {
             .finally(() => setIsLoading(false));
     }
 
+    const handleSearch = (e) => {
+        setSearch(e.target.value);
+    };
+
+    const debouncedSearch = useMemo(() => {
+        return debounce(handleSearch, 300);
+    }, []);
+
     useEffect(() => {
         listProducts(0);
-    }, []);
+
+        return () => {
+            debouncedSearch.cancel();
+        };
+    }, [search]);
 
     const columns = [
         { field: 'id', headerName: 'ID', width: 100}
@@ -47,19 +61,29 @@ const ProductListPage = () => {
 
     return (
         <Box sx={{ pt: 5 }}>
-            <Typography variant="h3" sx={{ mb: 5 }}>Products</Typography>
-                <DataGrid
-                    autoHeight
-                    rows={products}
-                    columns={columns}
-                    rowCount={total}
-                    loading={isLoading}
-                    pageSizeOptions={[10]}
-                    paginationModel={paginationModel}
-                    paginationMode="server"
-                    onRowClick={(row) => navigate(routes.product.replace(':id', row.id))}
-                    onPaginationModelChange={(e) => listProducts(e.page)}
-                />
+            <Typography variant="h3" sx={{ mb: 5 }}>
+                Products
+            </Typography>
+            <TextField
+                fullWidth
+                label="Search"
+                id="search"
+                onChange={debouncedSearch}
+                sx={{ mb: 3, mt: 1 }}
+            />
+
+            <DataGrid
+                autoHeight
+                rows={products}
+                columns={columns}
+                rowCount={total}
+                loading={isLoading}
+                pageSizeOptions={[10]}
+                paginationModel={paginationModel}
+                paginationMode="server"
+                onRowClick={(row) => navigate(routes.product.replace(':id', row.id))}
+                onPaginationModelChange={(e) => listProducts(e.page)}
+            />
         </Box>
     );
 };
--- src/services/api/apiClient/index.js
+++ src/services/api/apiClient/index.js
@@ -11,18 +11,12 @@ export default {
             const path = `/products/${id}`;
             return dummyJsonRequest('PUT', path, payload);
         },
-        list(skip, limit, select) {
+        list(search, skip, limit, select) {
             const query = {
+                q: search,
                 limit: limit || process.env.REACT_APP_LIMIT_PRODUCT_PER_PAGE,
                 skip: skip || 0,
                 select: select || process.env.REACT_APP_PRODUCT_LIST_CELLS
-            };
-            const path = `/products?${qs.stringify(query)}`;
-            return dummyJsonRequest('GET', path);
-        },
-        search(value) {
-            const query = {
-                q: value
             };
             const path = `/products/search?${qs.stringify(query)}`;
             return dummyJsonRequest('GET', path);
